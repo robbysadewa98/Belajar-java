6fba661051be613ed5224384baf3d53e2f1e843b
robbysadewa98 <robbysadewa98@gmail.com>
2020-10-24 16:24:54 +0700

    first commit

---
+++ TestNgoding.java
@@ -0,0 +1,15 @@
+public class TestNgoding{//ini class
+
+public static void main(String... argument){//ini function
+
+String nama="Dewa";
+String notlp="082114570869";
+byte umur=22; 
+
+System.out.println(nama+" - "+umur+" - "+notlp);//code logic
+
+}
+
+}
+
+
